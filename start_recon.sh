865b8a44bb65d6425a9f1e07d430d0e17166924f
Nick <73262982+nicholasc0523@users.noreply.github.com>
2024-11-07 21:00:26 -0500

    Update start_recon.sh

--- start_recon.sh
+++ start_recon.sh
@@ -11,7 +11,7 @@ fi
 # Get the absolute path of the current script (start_recon.sh)
 SCRIPT_DIR=$(dirname "$(realpath "$0")")
 
-# Define the full path to the main script (recon2.sh)
+# Define the full path to the main script
 RECON_SCRIPT="$SCRIPT_DIR/rt_recon.sh"
 
 # Use exec -a to rename the process and start the main script
